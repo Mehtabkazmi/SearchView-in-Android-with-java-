ae0d4d5b1ba80b841f33ffab70f3a56858bc33d8
Mehtabkazmi <mehtabkazmi5@gmail.com>
2021-07-08 00:31:25 +0500

    Android SearchView in java

---
+++ app/src/main/java/com/example/searchview/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.searchview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    SearchView searchView;
+    ListView listView;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchView=findViewById(R.id.searchview);
+        listView=findViewById(R.id.lv1);
+        list=new ArrayList<>();
+        list.add("apple");
+        list.add("banana");
+        list.add("orange");
+        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (list.contains(query)){
+                    adapter.getFilter().filter(query);
+
+                }else {
+                    Toast.makeText(MainActivity.this, "no match found", Toast.LENGTH_SHORT).show();
+                }
+            return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+}
